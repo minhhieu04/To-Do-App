261d027183ebdc353288c5fd08b55ad8e9b08a7f
minhhieu04 <99400715+minhhieu04@users.noreply.github.com>
2024-03-11 07:10:02 +0700

    #1 - Created login feature

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cors, { CorsOptions } from 'cors'
 import { dbConnection } from './config/dbconnect'
 import session from 'express-session'
 import { config } from 'dotenv'
+import authRoutes from './routes/auth.routes'
 
 config()
 
@@ -19,16 +20,23 @@ const PORT = process.env.PORT || 3000
 express.json({})
 // Sử dụng body-parser để lấy dữ liệu từ form
 app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use(
   session({
     secret: 'secret',
-    resave: true,
-    saveUninitialized: true
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false, // true: https
+      maxAge: 60 * 60 * 1000 // 60 minutes
+    }
   })
 )
 app.use(bodyParser.json())
 app.use(cors(corsOptions))
 app.use(express.static(path.join('public')))
+
+app.use('/auth', authRoutes)
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
